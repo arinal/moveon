6ecfed5e25d4ee7210f571e70a63cb273d60ea78
arinal <dicky.arinal@gmail.com>
2017-10-25 00:59:00 +0700

    Added README.org

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,14 +11,13 @@ object Dependencies {
     "io.circe"                 %% "circe-parser"    % circeVersion,
     "ch.qos.logback"           %  "logback-classic" % "1.1.2",
 
+    "mysql"              %  "mysql-connector-java" % "5.1.34",
+    "com.h2database"     %  "h2"                   % "1.4.185",
     "com.typesafe.slick" %% "slick"                % "3.2.1",
     "com.typesafe.slick" %% "slick-hikaricp"       % "3.2.1",
-    "mysql"              %  "mysql-connector-java" % "5.1.34",
     "com.typesafe.akka"  %% "akka-http"            % "10.0.10",
 
     "com.typesafe.akka" %% "akka-http-testkit" % "10.0.10" % Test,
-    "com.h2database"    %  "h2"                % "1.4.185",
-    // "com.h2database"    %  "h2"                % "1.4.185" % Test,
     "org.scalatest"     %% "scalatest"         % "3.0.1"   % Test
   )
 }
